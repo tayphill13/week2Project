0eefe0563dea863d31a799bc6a951a3acbc45247
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-05 13:11:15 -0700

    add second result if user selects chocBar.  Also moved form headers outside of previous div

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
   $("form#formOne").submit(function()  {
-    let userInput = $("input:radio[name=donutChoice]:checked").val();
-    console.log(userInput);
-    if (userInput) {
+    const userInput = $("input[name=donutChoice]:checked").val();
+    
+    if (userInput === "fritter") {
       result = "You would probably enjoy this programming language"; 
+    } else if (userInput === "chocBar") {
+      result = "You would probably enjoy this language instead";
     }
     
     $("#response").text(result);

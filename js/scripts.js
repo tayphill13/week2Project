205f2706feb78c657d63d3f5d80811b03c877e89
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-05 14:55:36 -0700

    add custom responses to be displayed to the user and I've confirmed that each work as intended.

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,13 @@ $(document).ready(function() {
     const morningOrNight = $("#amPm").val();
     
     if (preferredDonut === "fritter") {
-      $("#response").text("You would probably enjoy this programming language"); 
+      $("#response").text("Ever heard of C#??  Developed by Microsoft as part of it's .net initiative.  The most recent version, 8.0 was released in 2019 alongside Visual Studio 2019 version 16.3"); 
     } else if (preferredDonut === "chocBar") {
-      $("#response").text("You would probably enjoy this language instead");
-    } else {
-      $("#response").text("You would probably prefer this third language");
+      $("#response").text("Try Python! It's a high-level, general-purpose programming language that was first released in 1991.");
+    } else if (preferredDonut === "creme")  {
+      $("#response").text("How about looking into the Ruby, programming language?  It was developed in the mid-90's by Yukihiro 'Matz' Matsumoto in Japan, who thought that it would help programmers focus on providing what the user needs, not what the computer needs.");
+    } else  {
+      $("#response").text("Old-Fashioned eh?  Maybe start out with some HTML and CSS to get the basics first.  Then try to implement some JavaScript!");
     }
     event.preventDefault();
   })

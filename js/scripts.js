66cd5c986a67a9a7a37ef7b9eaa8c3f4a1ae37d0
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-05 11:02:58 -0700

    add the beginnings of my javscript, but it is not collecting info from the form yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
-  $("form#formOne").submit(function(event)  {
+  $("form#formOne").submit(function()  {
     event.preventDefault();
-    
+
+    let userInput = $("#donut").val();
+    let cocktail = $("input[name=mixedDrink]:checked").val();
+    console.log(userInput);
+    console.log(cocktail);
+    $("#response").text(userInput);
   })
 })
